03fb2b670ec0facb7c695c5810ce903c5c6e1c0b
Bruno Mello <bruno.barros.mello@gmail.com>
2024-03-28 17:05:12 -0300

    feat: add regex symbol type and symbol class

--- src/regex/regex_lexer.py
+++ src/regex/regex_lexer.py
@@ -1,4 +1,4 @@
-from src.utils.symbol import Symbol
+from src.utils.symbol import Symbol, SymbolType
 
 
 class RegexLexer:
@@ -9,41 +9,48 @@ class RegexLexer:
     # TODO: deve retornar ILLEGAL pra caso de simbolo inválido
     def next_symbol(self) -> Symbol:
         if self.pos >= len(self.input):
-            return Symbol.EOF
+            return Symbol(SymbolType.EOF, SymbolType.EOF.value)
 
         if self.input[self.pos] == "[":
             return self._handle_regex_shortcut()
 
-        if self.input[self.pos] == Symbol.OPEN_PARENTHESIS.value:
+        if self.input[self.pos] == SymbolType.OPEN_PARENTHESIS.value:
             self.pos += 1
-            return Symbol.OPEN_PARENTHESIS
+            return Symbol(SymbolType.OPEN_PARENTHESIS,
+                          SymbolType.OPEN_PARENTHESIS.value)
 
-        if self.input[self.pos] == Symbol.CLOSE_PARENTHESIS.value:
+        if self.input[self.pos] == SymbolType.CLOSE_PARENTHESIS.value:
             self.pos += 1
-            return Symbol.CLOSE_PARENTHESIS
+            return Symbol(SymbolType.CLOSE_PARENTHESIS,
+                          SymbolType.CLOSE_PARENTHESIS.value)
 
-        if self.input[self.pos] == Symbol.OR.value:
+        if self.input[self.pos] == SymbolType.OR.value:
             self.pos += 1
-            return Symbol.OR
+            return Symbol(SymbolType.OR, SymbolType.OR.value)
 
-        if self.input[self.pos] == Symbol.STAR.value:
+        if self.input[self.pos] == SymbolType.STAR.value:
             self.pos += 1
-            return Symbol.STAR
+            return Symbol(SymbolType.STAR, SymbolType.STAR.value)
 
-        if self.input[self.pos] == Symbol.PLUS.value:
+        if self.input[self.pos] == SymbolType.PLUS.value:
             self.pos += 1
-            return Symbol.PLUS
+            return Symbol(SymbolType.PLUS, SymbolType.PLUS.value)
+
+        return Symbol(SymbolType.ILLEGAL, self.input[self.pos])
 
     def _handle_regex_shortcut(self) -> Symbol:
         inner_content = self.input[self.pos+1: self.pos+4]
 
         self.pos += 5
 
-        if inner_content == "A-z":
-            return Symbol.TEXT
-        elif inner_content == "A-Z":
-            return Symbol.UPPER
-        elif inner_content == "a-z":
-            return Symbol.LOWER
-        elif inner_content == "0-9":
-            return Symbol.NUMBER
+        content = f"[{inner_content}]"
+
+        if content == "[A-z]":
+            return Symbol(SymbolType.TEXT, content)
+        elif content == "[A-Z]":
+            return Symbol(SymbolType.UPPER, content)
+        elif content == "[a-z]":
+            return Symbol(SymbolType.LOWER, content)
+        elif content == "[0-9]":
+            return Symbol(SymbolType.NUMBER, content)
+        return Symbol(SymbolType.ILLEGAL, content)
--- test/regex/regex_lexer_test.py
+++ test/regex/regex_lexer_test.py
@@ -1,51 +1,57 @@
 from src.regex.regex_lexer import RegexLexer
-from src.utils.symbol import Symbol
+from src.utils.symbol import Symbol, SymbolType
 
 
 def test_next_symbol():
-    input = "([A-z]|[A-Z])[0-9]*[a-z]+"
+    input = "([A-z]|[A-Z])[0-9]*[a-z]+[xyz]"
     expected_symbols = [
-        Symbol.OPEN_PARENTHESIS,
-        Symbol.TEXT,
-        Symbol.OR,
-        Symbol.UPPER,
-        Symbol.CLOSE_PARENTHESIS,
-        Symbol.NUMBER,
-        Symbol.STAR,
-        Symbol.LOWER,
-        Symbol.PLUS,
-        Symbol.EOF,
-        Symbol.EOF,
-        Symbol.EOF,
+        Symbol(SymbolType.OPEN_PARENTHESIS, SymbolType.OPEN_PARENTHESIS.value),
+        Symbol(SymbolType.TEXT, SymbolType.TEXT.value),
+        Symbol(SymbolType.OR, SymbolType.OR.value),
+        Symbol(SymbolType.UPPER, SymbolType.UPPER.value),
+        Symbol(SymbolType.CLOSE_PARENTHESIS,
+               SymbolType.CLOSE_PARENTHESIS.value),
+        Symbol(SymbolType.NUMBER, SymbolType.NUMBER.value),
+        Symbol(SymbolType.STAR, SymbolType.STAR.value),
+        Symbol(SymbolType.LOWER, SymbolType.LOWER.value),
+        Symbol(SymbolType.PLUS, SymbolType.PLUS.value),
+        Symbol(SymbolType.ILLEGAL, "[xyz]"),
+        Symbol(SymbolType.EOF, SymbolType.EOF.value),
+        Symbol(SymbolType.EOF, SymbolType.EOF.value),
+        Symbol(SymbolType.EOF, SymbolType.EOF.value),
     ]
     lexer = RegexLexer(input)
 
     for expected_symbol in expected_symbols:
         symbol = lexer.next_symbol()
-        assert symbol == expected_symbol
+        if symbol is None:
+            print(expected_symbol.value, expected_symbol.type)
+        assert symbol.type == expected_symbol.type
+        assert symbol.value == expected_symbol.value
 
 
 def test_regex_shortcut():
     tests = [
         {
             "input": "[A-Z]",
-            "expected": Symbol.UPPER
+            "expected": Symbol(SymbolType.UPPER, SymbolType.UPPER.value)
         },
         {
             "input": "[a-z]",
-            "expected": Symbol.LOWER
+            "expected": Symbol(SymbolType.LOWER, SymbolType.LOWER.value)
         },
         {
             "input": "[A-z]",
-            "expected": Symbol.TEXT
+            "expected": Symbol(SymbolType.TEXT, SymbolType.TEXT.value)
         },
         {
             "input": "[0-9]",
-            "expected": Symbol.NUMBER
+            "expected": Symbol(SymbolType.NUMBER, SymbolType.NUMBER.value)
         }
     ]
 
     for test in tests:
         lexer = RegexLexer(test["input"])
         symbol = lexer._handle_regex_shortcut()
-        assert symbol == test["expected"]
+        assert symbol.type == test["expected"].type
+        assert symbol.value == test["expected"].value
