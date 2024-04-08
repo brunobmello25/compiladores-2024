63617c3026a7967218a04d8345842841ca89d884
RodrigoGCorrea <rodrigogc@id.uff.br>
2024-04-08 11:57:40 -0300

    fix: removing final state

--- test/regex/regex_parser_test.py
+++ test/regex/regex_parser_test.py
@@ -34,10 +34,10 @@ def test_parse_parenthesis_regex():
     result_states_names = {state.name for state in result.states}
     result_accept_states_names = {state.name for state in result.accept_states}
 
-    assert len(result.states) == 9
-    assert result_states_names == {"q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8"}
+    assert len(result.states) == 8
+    assert result_states_names == {"q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7"}
     assert result.start_state.name == "q0"
-    assert result_accept_states_names == {"q8"}
+    assert result_accept_states_names == {"q7"}
     assert result.check_transition_by_state_name("q0", "q1", None)
     assert result.check_transition_by_state_name("q1", "q2", None)
     assert result.check_transition_by_state_name("q2", "q3", None)
@@ -45,5 +45,4 @@ def test_parse_parenthesis_regex():
     assert result.check_transition_by_state_name("q4", "q5", None)
     assert result.check_transition_by_state_name("q5", "q6", None)
     assert result.check_transition_by_state_name("q6", "q7", "b")
-    assert result.check_transition_by_state_name("q7", "q8", None)
-    assert len(result.transition_function.keys()) == 8
+    assert len(result.transition_function.keys()) == 7
